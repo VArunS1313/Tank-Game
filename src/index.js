39001865d8bde84e37eb8943bd55ab337bd93d63
VArunS1313 <vs1313046@gmail.com>
2023-12-09 23:35:54 +0530

    first

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const { createServer } = require("http");
+const { Server, Socket } = require("socket.io");
+
+const app=express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connect",(socket)=>{
+console.log("user connected "+socket.id);
+});
+
+app.use(express.static("public"));
+
+
+
+httpServer.listen(5000);
